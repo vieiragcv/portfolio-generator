801eec72534bbe492ceed4b2071fa64719885225
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-02-19 23:29:02 -0500

    multiple project creation

--- app.js
+++ app.js
@@ -1,26 +1,84 @@
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
+/*-----------------------------------------------------------------------------
+-                               REQUEST USER INFO
+-----------------------------------------------------------------------------*/
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
     }
   ])
-  .then(answers => console.log(answers));
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+  });
+};
 
-/* const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+/*-----------------------------------------------------------------------------
+-                               CREATE PROJECT
+-----------------------------------------------------------------------------*/
+const promptProject = portfolioData => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  console.log(`
+    =================
+    Add a New Project
+    =================
+  `);
+  return inquirer.prompt([  {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
 
+/*-----------------------------------------------------------------------------
+-                               RUN APP
+-----------------------------------------------------------------------------*/
 
-const pageHTML = generatePage(name1, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-*/
-
- 
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
