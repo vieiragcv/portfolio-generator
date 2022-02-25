ba7b8d05dd0873a4a8203ba02df580f1b32e88e1
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-02-24 21:12:34 -0500

    finished exercise

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 /*-----------------------------------------------------------------------------
 -                             REQUEST USER INFO
